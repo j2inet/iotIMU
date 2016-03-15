896219b6e32916aa590c93d9af9e974eb160deec
Joel Ivory Johnson <joelijohnson@hotmail.com>
2016-03-15 17:52:07 -0400

    detailed messaging

--- IMURelay/src/IMURelay.cpp
+++ IMURelay/src/IMURelay.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <ostream>
 #include <lsm9ds0.h>
+#include <time.h>
+
 using namespace std;
 
 
@@ -14,22 +17,50 @@ struct Vector {
 	float z;
 };
 
+struct MotionReading
+{
+	Vector accelReading;
+	Vector gyroReading;
+	Vector magReading;
+	float temperature;
+	tm time;
+};
+
+inline std::ostream& operator<< (std::ostream& stream,  const MotionReading& m) {
+
+	stream << "{ "
+			<< "   mag:  { x:" << m.magReading.x   << ", y:" << m.magReading.y   << ", z:" << m.magReading.z  << "},"
+			<< "   gyro: { x:" << m.gyroReading.x  << ", y:" << m.gyroReading.y  << ", z:" << m.gyroReading.z << "},"
+			<< "   accel:{ x:" << m.accelReading.x << ", y:" << m.accelReading.y << ", z:" << m.gyroReading.z << "},"
+			<< "   temp:" <<  m.temperature
+			<< "   time:" << time
+			<< "}";
+	return stream;
+}
+
 
 int main() {
   /* Setup your example here, code that should run once
    */
 
+	MotionReading reading;
 	motionSensor.init();
 
   /* Code in this loop will run repeatedly
    */
   for (;;) {
-	  Vector magReading, accelReading, gyroReading;
+
 
 	  motionSensor.update();
-	  motionSensor.getMagnetometer(&magReading.x, &magReading.y, &magReading.z);
-	  motionSensor.getAccelerometer(&accelReading.x, &accelReading.y, &accelReading.z);
-	  motionSensor.getGyroscope(&gyroReading.x, &gyroReading.y, &gyroReading.z);
+	  motionSensor.getMagnetometer(&reading.magReading.x, &reading.magReading.y, &reading.magReading.z);
+	  motionSensor.getAccelerometer(&reading.accelReading.x, &reading.accelReading.y, &reading.accelReading.z);
+	  motionSensor.getGyroscope(&reading.gyroReading.x, &reading.gyroReading.y, &reading.gyroReading.z);
+	  reading.temperature = motionSensor.getTemperature();
+	  time_t currentTime;
+	  time(&currentTime);
+	  reading.time = localtime(&currentTime);
+
+	  cout << reading << endl;
   }
 
   return 0;
